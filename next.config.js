6b4e64a100e5706eee7acb023cfbee456fcc8713
dwivediansh7 <dwivediansh0@gmail.com>
2025-09-18 00:07:00 +0530

    Fix 2

---
+++ next.config.js
@@ -0,0 +1,17 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@genkit-ai/core', '@genkit-ai/googleai']
+  }
+};
+
+module.exports = nextConfig;
